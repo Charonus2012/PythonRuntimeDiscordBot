12a8ef8cf5a6758f761186fc0530b57a2c2f04c9
Patrik <patrikpilatos@gmail.com>
2024-07-17 19:58:12 +0200

    Disabled some python features

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import discord
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
 
+def check(code) -> bool:
+    for i in rest_commands:
+        if code.count(i) > 0:
+            return False
+    return True
+def err_message(code):
+    for i in rest_commands:
+        if code.count(i) > 0:
+            return i
+    return None
+
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL = os.getenv("CHANNEL")
@@ -12,6 +24,7 @@ async def send_message(message: str, channel: discord.TextChannel):
     await channel.send(message)
 
 
+rest_commands = ["open(", "input(", "os", "pandas", "tkinter", "pygame", "winreg"]
 # Setup
 intents: Intents = Intents.default()
 intents.message_content = True  # NOQA
@@ -36,9 +49,12 @@ async def on_message(message: Message) -> None:
         lang: str = content.split("```")[1].split("\n")[0]
         code: str = "\n".join(content.split("```")[1].split("\n")[1:])
         if lang == "python":
-            with open("code.py", "w") as f:
-                f.write(code)
-            os.system("code.py > output.txt")
+            if check(code):
+                with open("code.py", "w") as f:
+                    f.write(code)
+                os.system("code.py > output.txt")
+            else:
+                os.system(f"echo Your program cant use keyword {err_message(code)} > output.txt")
             with open("output.txt", "r") as f:
                 output: str = f.read()
             final_output: str = f"```\n{output}```"
